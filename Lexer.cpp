121d2e291b572885ac947eaec39ab57f95b2f4a3
Farida-Fakhry02 <21P0167@eng.asu.edu.eg>
2024-03-27 15:34:00 +0200

    Operators

--- Lexer.cpp
+++ Lexer.cpp
@@ -27,9 +27,153 @@ bool is_reserved(string identifier) {
   return false;
 
 }
+int add_to_symbol_table(std::string token_name, int offset) {
+int index = 0;
+ for (auto& symbol : symbol_table) {
+ if (token_name == symbol.first) {
+ symbol.second.push_back(offset);
+ return index;
+ }
+ index++;
+ }
+ symbol_table.push_back({ token_name, { offset } });
+ return index;
+ }
+
+ std::ostream& operator<<(std::ostream& out, const symbol_table_t& st) {
+ std::string str = "\n\nSYMBOL TABLE\n\n";
+ int index = 0;
+ for (auto const symbol : symbol_table) {
+ str += std::to_string(index++) + "- Symbol: " + symbol.first + ", at offset(s): ";
+ for (auto offset : symbol.second) str += std::to_string(offset) + " ";
+ str += "\n";
+ }
+ out << str;
+ return out;
+ }
+
+
+bool get_next_token_regex(std::string& buffer, token*& token) {
+
+	static std::regex all_operators(
+		"\\+\\+|--"                // Unary arithmetic operators (++, --)
+		"|\\+|-|\\*|/|%"           // Binary arithmetic operators (+, -, *, /, %)
+		"|>|<|==|>=|<="            // Binary relational operators (>, <, ==, >=, <=)
+		"|=|\\*=|%=|\\+=|-=|/="    // Binary assignment operators (=, *=, %=,/=, +=, -=)
+		"|&&|\\|\\||!|<<|>>"       // Binary logical operators (&&, ||, !, <<, >>)
+		"|&|\\||~"                 // Binary bitwise operators (&, |, ~)
+		"|\\*|&|->"                // Pointer operations (*, &, ->)
+		"|\\."                     // Member operations (.)
+		"|\\?"                     // Ternconditional operator ()
+    "|\\(|\\)"                 // Parentheses
+    "|;"                       // Semicolon
+    "|:"
+    "|\\{|\\}"                  // Braces
+    "\\[|\\]"                  // Brackets
+	);
+
+	static std::sregex_iterator my_regex_iterator(
+		buffer.begin(),
+		buffer.end(),
+		all_operators
+	);
+	static std::sregex_iterator end;
+	while (my_regex_iterator != end) {
+		std::string match = my_regex_iterator->str();
+		int lexeme_begin = my_regex_iterator->position();
+		++my_regex_iterator;
+
+		if (match.length() == 1) {
+			switch (match[0]) { // We can't switch-case on a string, so we get the first character in it
+			case '=': {cout << "=";  return token->type = ASSIGN_OP;};
+			case '<': return token->type = SMALLER_THAN_OP;
+			case '>': return token->type = GREATER_THAN_OP;
+			case '+': return token->type = ADD_OP;
+			case '-': return token->type = SUB_OP;
+			case '*': return token->type = ASTERISK_OP;
+			case '/': return token->type = DIV_OP;
+			case '%': return token->type = REM_OP;
+      case '!': return token->type = LOGICAL_NOT_OP;
+      case '&': return token->type = BITWISE_AND_OP;
+      case '|': return token->type = BITWISE_OR_OP;
+      case '~': return token->type = BITWISE_NOT_OP;
+      case '.': return token->type = MEMBER_OP;
+      case '?': return token->type = TERNARY_OP;
+      case '(': return token->type = LEFT_BRACKET;
+      case ')': return token->type = RIGHT_BRACKET;
+			case ';': return token->type = SEMICOLON;
+      case ':': return token->type = COLON_OP;  
+      case ',': return token->type = COMMA_OP;
+      case '[': return token->type = LEFT_SQUARE_BRACKET;
+      case ']': return token->type = RIGHT_SQUARE_BRACKET;
+      case '{': return token->type = LEFT_CURLY_BRACKET;
+      case '}': return token->type = RIGHT_CURLY_BRACKET;
+			case ' ': continue;
+			case '\n': continue;
+			case '\t': continue;
+			case EOF: return token->type = END_OF_FILE;
+      default: {cout << "Error";  return token->type = ERROR;}
+			}
+		}
+    if (std::regex_match(match, std::regex("\\{[^\\{\\}]+\\}"))) {
+			token->name = match;
+			return token->type = BLOCK_COMMENT;
+		}
+    if (match.length() == 2){
+      if (match == "++") 
+			  return token->type = INCREMENT_OP;
+
+      else if (match == "--")
+        return token->type = DECREMENT_OP;
+
+      else if (match == "&&")
+        return token->type = LOGICAL_AND_OP;
+
+      else if (match == "||")
+        return token->type = LOGICAL_OR_OP;
+
+      else if (match == "==")
+        return token->type= EQUALS_OP;
+
+      else if(match=="<=")
+        return token->type= SMALLER_THAN_EQ_OP;
+
+      else if(match==">=")
+        return token->type= GREATER_THAN_EQ_OP;
+
+      else if(match=="<<")
+        return token->type= SHIFT_LEFT_OP;
+
+      else if(match==">>")
+        return token->type= SHIFT_RIGHT_OP;
+
+      else if(match=="*=")
+        return token->type= MUL_EQUAL_OP;
+
+      else if(match=="/=")
+        return token->type= DIV_EQUAL_OP;
+
+      else if(match=="+=")
+        return token->type= ADD_EQUAL_OP;
+
+      else if(match=="-=")
+        return token->type= SUB_EQUAL_OP;
+
+      else if(match=="%=")
+        return token->type= MOD_EQUAL_OP;
+
+  }
+   
+  }
+  return false;
+}
 
 int main() {
 
+  cout << "hello";
+
+
+  token* token;
   string buf;
   read_file("Trial.txt", buf);
    
@@ -43,19 +187,29 @@ int main() {
         hashTable.insert(keyword);
   }
 
-  //test buffer
-  cout<<buf << endl;
-  //test hashtable
-  cout<< hashTable.size << endl;
+bool x= get_next_token_regex(buf, token);
+cout << x;
+ // Tokenize it
+//  token* tok;
+//  std::vector<token*> tokens;
+//  while (get_next_token_regex(buf, tok)) tokens.push_back(tok);
+//  // Print out the tokens and symbol table
+//  for (auto token : tokens) std::cout << *token << std::endl;
+//  std::cout << symbol_table << std::endl;
 
-  //test is reserved
+  // //test buffer
+  // cout<<buf << endl;
+  // //test hashtable
+  // cout<< hashTable.size << endl;
 
-  bool is = is_reserved("short");
-  cout << " short is: "<< is << endl;
-  is = is_reserved("hi");
-  cout << " hi is: "<< is << endl;
-  is = is_reserved("void");
-  cout << " void is: "<< is << endl;
+  // //test is reserved
+
+  // bool is = is_reserved("short");
+  // cout << " short is: "<< is << endl;
+  // is = is_reserved("hi");
+  // cout << " hi is: "<< is << endl;
+  // is = is_reserved("void");
+  // cout << " void is: "<< is << endl;
 
 
   return 0;
--- Token.cpp
+++ Token.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 
 enum token_type {
-END_OF_FILE = 0, ERROR,
-RESERVED_KW, IDENTIFER, NUMBER,
-ADD_OP, SUB_OP, MUL_OP, DIV_OP,
-EQUALS_OP, SMALLER_THAN_OP,
-ASSIGN_OP,
-LEFT_BRACKET, RIGHT_BRACKET, SEMICOLON,
-BLOCK_COMMENT
+    END_OF_FILE = 0, ERROR,
+    RESERVED_KW, IDENTIFER, NUMBER,
+    ADD_OP, SUB_OP, ASTERISK_OP, DIV_OP,
+    EQUALS_OP, SMALLER_THAN_OP,
+    SMALLER_THAN_EQ_OP, GREATER_THAN_OP, GREATER_THAN_EQ_OP, NOT_EQ_OP, BITWISE_AND_OP,
+    BITWISE_OR_OP, BITWISE_NOT_OP,
+    LOGICAL_AND_OP, LOGICAL_OR_OP, LOGICAL_NOT_OP,
+    ASSIGN_OP, REM_OP,
+    LEFT_BRACKET, RIGHT_BRACKET, SEMICOLON,
+    BLOCK_COMMENT,COLON_OP,
+    POINTER_DEC_OP,POINTER_REF_OP,POINTER_MEM_ACCESS_OP, MEMBER_OP, TERNARY_OP,INCREMENT_OP,DECREMENT_OP,
+    COMMA_OP,SHIFT_LEFT_OP,SHIFT_RIGHT_OP,ADD_EQUAL_OP,SUB_EQUAL_OP,MUL_EQUAL_OP,DIV_EQUAL_OP,MOD_EQUAL_OP,
+    LEFT_CURLY_BRACKET,RIGHT_CURLY_BRACKET,LEFT_SQUARE_BRACKET,RIGHT_SQUARE_BRACKET
+    
 };
 
 
@@ -25,3 +32,52 @@ public:
     int symbol_table_index;
     
 };
+
+ typedef std::vector<std::pair<std::string, std::vector<int>>> symbol_table_t;
+  symbol_table_t symbol_table;
+
+std::ostream& operator<<(std::ostream& out, const token& token) {
+ std::string str;
+ str += "<";
+ switch (token.type) {
+ case RESERVED_KW:
+ str += "reserved keyword, name = " + token.name;
+ break;
+ case IDENTIFER:
+ str += "identifier, name = " + token.name +
+ ", symbol table index #" + std::to_string(token.symbol_table_index);
+ break;
+ case NUMBER:
+ str += "number, value = " + std::to_string(token.value);
+  case ADD_OP:
+ str += "addition arithmetic operator";
+ break;
+ case SUB_OP:
+ str += "subtract arithmetic operator";
+ break;
+ case ASTERISK_OP:
+ str += "multiply arithmetic operator";
+ break;
+ case SEMICOLON:
+ str += "semicolon";
+ break;
+ case BLOCK_COMMENT:
+ str += "block comment (to be ignored), content = " + token.name;
+ break;
+ case END_OF_FILE:
+ str += "end-of-file";
+ break;
+ case ERROR:
+ str += "erroneous token, content = " + token.name;
+ break;
+ default:
+ str += "unexpected token";
+ break;
+ }
+ str += ">";
+ out << str;
+ return out;
+ }
+
+  
+ 
